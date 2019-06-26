39626ef8a1688bec35d793f2107c9e15abf144d2
Ahmad Helal <ahmadhelal@Ahmads-MacBook-Pro.local>
2019-06-26 11:05:35 +0300

    Can't call 1st function in my CLI

--- functions_task.py
+++ functions_task.py
@@ -5,16 +5,6 @@ current_date= date.today()
 print ("Today's date is ", current_date)
 
 
-if :
-
-	print ("you entered %s-%s-%s" %(birth_year, birth_month, birth_day))
-
-else:
-
-	print ("You enter wrong formats, please run the program again") 
-
-
-
 
 # define check_birthdate
 def check_birthdate (year, month, day):
@@ -31,7 +21,7 @@ def check_birthdate (year, month, day):
 
 	if  len (year)==4 and year.isnumeric() and len (month)<=2 and month.isnumeric() and len(day)<=2 and day.isnumeric() and check_birthdate(year, month, day)<=current_date and check_birthdate(year, month, day)>0:
 		#Check birth date is not in the future and additionally checks if date formate is correct
-		print (" True ")
+		return True
 
 	
 	# False message
@@ -39,12 +29,7 @@ def check_birthdate (year, month, day):
 	else:
 		print ("False. You entered values with wrong format; please enter your date of birth again \n ")
 		year, month, day= input ("Please enter your birthdate here (YYYY-MM-DD) : \n").split()
-	return check_birthdate
-
-
-check_birthdate (year, month, day)
-
-#[Previous line repeated 996 more times] RecursionError: maximum recursion depth exceeded
+	return False
 
 
 
