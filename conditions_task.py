25cfcfdfc4ba5b2d03f6f2b73d008b46ab5bbfda
Ahmad Helal <you@example.com>
2019-06-28 16:20:40 +0300

    Re-uploaded as instructed

--- conditions_task.py
+++ conditions_task.py
@@ -1,12 +1,17 @@
 
+#Define 2 variables and one operator
 var1= input("Please enter 1st integer: \n")
 var2= input("Please enter 2nd integer: \n")
+oper=input("Please enter an operator (+,-,*,/): \n")
 
-
-oper1=input("Please enter 1st operator (+,-,*,/): \n")
-oper2=input("Please enter 2nd operator (+,-,*,/): \n")
-
-if (var1.isdigit()==True and var2.isdigit() == True) and (oper1=="+" or oper1=="-" or oper1=="*" or oper1=="/") and (oper2=="+" or oper2=="-" or oper2=="*" or oper2=="/"):
-	print ("You entered two integers. Operator 1 is %s and operator 2 is %s" %(oper1, oper2))
+#Conditions
+if (var1.isdigit()==True and var2.isdigit() == True) and oper== "+":
+	print ("the result of %s %s %s=" %(var1,oper,var2), int(var1)+int(var2))
+elif (var1.isdigit()==True and var2.isdigit() == True) and oper=="*":
+	print ("the result of %s %s %s= "%(var1,oper,var2), int(var1)*int(var2))
+elif (var1.isdigit()==True and var2.isdigit() == True) and oper=="-":
+	print ("the result of %s %s %s=" %(var1,oper,var2), int(var1)-int(var2))
+elif (var1.isdigit()==True and var2.isdigit() == True) and oper=="/":
+	print ("the result of %s %s %s=" %(var1,oper,var2), int(var1)/int(var2))
 else:
 	print ("Operation is invalid \n")
